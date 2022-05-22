4ac917014d9d864443314adaec9f67b9256c9ccc
Oswaldo Peña <oswaldo@Oswaldo-Pro.local>
2022-05-22 10:13:18 -0500

    Fix error calling superclasses

--- futbolista.py
+++ futbolista.py
@@ -5,8 +5,8 @@ class Futbolista(Persona, Deportista):
     listaFutbolistas = []
 
     def __init__(self, nombre, edad, altura, sexo, añosPracticando, golesMarcados, tarjetasRojas, piernaHabil):
-        Persona.__init__(nombre, edad, altura, sexo)
-        Deportista.__init__("Futbol", añosPracticando)
+        Persona.__init__(self, nombre, edad, altura, sexo)
+        Deportista.__init__(self, "Futbol", añosPracticando)
         self._golesMarcados = golesMarcados
         self._tarjetasRojas = tarjetasRojas
         self._piernaHabil = piernaHabil
